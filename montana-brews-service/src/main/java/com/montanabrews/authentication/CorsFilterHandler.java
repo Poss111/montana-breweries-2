f621bac3fd2c0652cb9822c1c6dd88226941662c
Poss111 <dpatposs@msn.com>
2018-10-09 19:24:40 -0500

    Added changes to enable cors headers - Daniel

---
+++ montana-brews-service/src/main/java/com/montanabrews/authentication/CorsFilterHandler.java
@@ -0,0 +1,48 @@
+package com.montanabrews.authentication;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CorsFilterHandler implements Filter {
+
+	private static final Logger LOG = LoggerFactory.getLogger(CorsFilterHandler.class);
+	
+	@Override
+	public void destroy() {
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+			throws IOException, ServletException {
+		LOG.debug("Setting up CORS filters for requests...");
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		
+		httpResponse.setHeader("Access-Control-Allow-Origin", "*");
+		httpResponse.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
+		httpResponse.setHeader("Access-Control-Allow-Headers", "*");
+		httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
+		httpResponse.setHeader("Access-Control-Max-Age", "180");
+        filterChain.doFilter(httpRequest, httpResponse);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		
+	}
+
+}
--- montana-brews-service/src/main/java/com/montanabrews/configuration/SecurityConfig.java
+++ montana-brews-service/src/main/java/com/montanabrews/configuration/SecurityConfig.java
@@ -8,7 +8,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.session.SessionManagementFilter;
 
+import com.montanabrews.authentication.CorsFilterHandler;
 import com.montanabrews.authentication.CustomAccessDeniedHandler;
 import com.montanabrews.authentication.RestAuthenticationEntryPoint;
 import com.montanabrews.constants.MontanaBrewsAPIConstants;
@@ -25,11 +27,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private CustomAccessDeniedHandler customAccessDeniedHandler;
 
+	@Autowired
+	private CorsFilterHandler corsFilterHandler;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.antMatcher(MontanaBrewsAPIConstants.PRIVATE_API_FILTER).authorizeRequests().anyRequest().authenticated()
-				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().httpBasic()
-				.and().exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint)
+		http.addFilterBefore(corsFilterHandler, SessionManagementFilter.class)
+				.antMatcher(MontanaBrewsAPIConstants.PRIVATE_API_FILTER).authorizeRequests().anyRequest()
+				.authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.httpBasic().and().exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint)
 				.accessDeniedHandler(customAccessDeniedHandler).and().csrf().disable();
 	}
 
