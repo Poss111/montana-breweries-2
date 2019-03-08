75e6ccee610e88e41859d23ac700b4c31e1c29a0
Poss111 <dpatposs@msn.com>
2019-03-08 15:26:11 -0600

    Commenting unnecessary codes - Daniel

--- montanabreweries2ui/src/app/app.component.ts
+++ montanabreweries2ui/src/app/app.component.ts
@@ -13,58 +13,59 @@ import { filter } from 'rxjs/operators';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [FilterService]
+  // providers: [FilterService]
 })
-export class AppComponent implements OnInit {
-  title = 'montanabreweries2ui';
+export class AppComponent {}
+//  implements OnInit {
+  // title = 'montanabreweries2ui';
 
-  @ViewChildren(FiltersComponent)
-  private filtersComponent: FiltersComponent[];
+  // @ViewChildren(FiltersComponent)
+  // private filtersComponent: FiltersComponent[];
 
-  private datasubscription: Subscription;
+  // private datasubscription: Subscription;
 
-  private statesubscription: Subscription;
+  // private statesubscription: Subscription;
 
-  microbrews: Microbrew[];
+  // microbrews: Microbrew[];
 
-  filterMaps: FilterMap[] = [];
+  // filterMaps: FilterMap[] = [];
 
-  constructor(private microbrewService : MicrobrewService, private websocketService: WebsocketService, private filterService: FilterService) {}
+  // constructor(private microbrewService : MicrobrewService, private websocketService: WebsocketService, private filterService: FilterService) {}
 
-  ngOnInit() {
-    this.getMicrobrews();
-    this.websocketService.connectWebSocket();
-    this.datasubscription = this.websocketService.getSocketDataObservable().subscribe(this.onData);
-    this.statesubscription = this.websocketService.getSocketStateObservable().subscribe(this.onStateChange);
-    this.filterService.getFilter().subscribe( filtering => {
-      this.filterMaps.push(filtering);
-    });
-    // this.getMicrobrews();
-  }
+  // ngOnInit() {
+  //   this.getMicrobrews();
+  //   this.websocketService.connectWebSocket();
+  //   this.datasubscription = this.websocketService.getSocketDataObservable().subscribe(this.onData);
+  //   this.statesubscription = this.websocketService.getSocketStateObservable().subscribe(this.onStateChange);
+  //   this.filterService.getFilter().subscribe( filtering => {
+  //     this.filterMaps.push(filtering);
+  //   });
+  //   // this.getMicrobrews();
+  // }
 
-  getMicrobrews(): void {
-    console.log("Calling Microbrews Service...");
-    this.microbrewService.getMicrobrews().subscribe(microbrews => this.microbrews = microbrews);
-  }
+  // getMicrobrews(): void {
+  //   console.log("Calling Microbrews Service...");
+  //   this.microbrewService.getMicrobrews().subscribe(microbrews => this.microbrews = microbrews);
+  // }
 
-  private onData = (message: Message) => {
-    this.microbrews = JSON.parse(message.body);
-  }
+  // private onData = (message: Message) => {
+  //   this.microbrews = JSON.parse(message.body);
+  // }
 
-  private onStateChange = (state: String) => {
-    console.log('WS connection State has changed :: ' + state);
-  }
+  // private onStateChange = (state: String) => {
+  //   console.log('WS connection State has changed :: ' + state);
+  // }
 
-  submitFilters() {
-    this.filtersComponent.forEach(function (filterComponent) {
-      filterComponent.submitFilter();
-    });
-  }
+  // submitFilters() {
+  //   this.filtersComponent.forEach(function (filterComponent) {
+  //     filterComponent.submitFilter();
+  //   });
+  // }
 
-  ngOnDestroy() {
-    this.datasubscription.unsubscribe();
-    this.statesubscription.unsubscribe();
-  }
+  // ngOnDestroy() {
+  //   this.datasubscription.unsubscribe();
+  //   this.statesubscription.unsubscribe();
+  // }
 
 }
 
