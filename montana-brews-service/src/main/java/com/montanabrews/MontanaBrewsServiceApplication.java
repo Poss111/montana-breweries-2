cf7870bc6adba8b3e27e480ce992b04acb4d4037
Poss111 <dpatposs@msn.com>
2019-06-02 01:49:25 -0500

    Updating to use external Spring Config Server - Daniel

--- montana-brews-service/src/main/java/com/montanabrews/MontanaBrewsServiceApplication.java
+++ montana-brews-service/src/main/java/com/montanabrews/MontanaBrewsServiceApplication.java
@@ -1,14 +1,18 @@
 package com.montanabrews;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class MontanaBrewsServiceApplication {
 	
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(MontanaBrewsServiceApplication.class);
-		app.run(args);
+		for (String arg: args) {
+			System.out.println(arg);
+		}
+		SpringApplication.run(MontanaBrewsServiceApplication.class, args);
 	}
 	
 }
